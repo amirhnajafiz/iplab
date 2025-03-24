081b917ac285dc6d9ec097fa66394c11df503263
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-23 21:54:46 -0400

    update: background image

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -44,10 +44,10 @@ document.addEventListener("DOMContentLoaded", function () {
     </table>
     `;
 
-  const map = L.map("map").setView([data.latitude, data.longitude], 13);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  const map = L.map("map").setView([data.latitude, data.longitude], 5);
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   }).addTo(map);
   L.marker([data.latitude, data.longitude])
     .addTo(map)
